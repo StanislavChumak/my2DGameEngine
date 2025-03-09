a67daef44551c809ce4acba304b421469b6892a5
Stanislav <stanislav.chumak.23@gmail.com>
2025-03-10 01:44:16 +0400

    delete

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stas/programming/C++/projectOpenGL/newProjectOpenGL
-# Build directory: /home/stas/programming/C++/projectOpenGL/newProjectOpenGL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
